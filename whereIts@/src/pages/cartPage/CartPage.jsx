d57e48ac0e54fb1ea070e2aa65eaafac931a47a1
Anton Knutsson <cykelkorg95@gmail.com>
2024-05-07 14:47:42 +0200

    can now calc total cost in real time

--- whereIts@/src/pages/cartPage/CartPage.jsx
+++ whereIts@/src/pages/cartPage/CartPage.jsx
@@ -9,15 +9,29 @@ function CartPage() {
 
     const {cartItems} = useContext(CartContext);
 
+
+    const calcTotalCost = () => {
+      let totalCost = 0;
+      cartItems.forEach(item => {
+        totalCost += item.event.price * item.quantity;
+      });
+      console.log(calcTotalCost, 'Total kostnad');
+      return totalCost;
+    }
+
     console.log(cartItems, 'CartPage');
   return (
     <section className='cart-page__wrapper'>
         <h2 className='cart-page__title'>Order</h2>
-        <ul>
+        <ul className='cart-item__container'>
             {cartItems.map((item, index) => (
                 <CartCard key={index} item={ item }/>
             ))}
         </ul>
+        <section className='price-section'>
+          <p className='cart-page__price-text'>Totalt värde på order:</p>
+          <p className='cart-page__price'>{calcTotalCost()}</p>
+        </section>
     </section >
   )
 }
