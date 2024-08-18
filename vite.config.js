553b5ef26c156c6ace5c9aabe54cfc9386eebc3b
Anton Knutsson <cykelkorg95@gmail.com>
2024-08-18 13:26:18 +0200

    deploy yaml file and vite.config changes

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/WhereIts--indExam/',  // Viktigt för GitHub Pages
-  // build: {
-  //   rollupOptions: {
-  //     input: {
-  //       main: './index.html',
-  //       '404': './index.html',  // Skapa en 404.html som omdirigerar till index.html
-  //     }
-  //   }
-  // }
+  base: '/WhereIts--indExam/'  // Viktigt för GitHub Pages
 })
